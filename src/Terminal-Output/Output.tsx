8a7c0af7fde84eede8da10ee83b39c4a790b9790
edincitaku <edin.citaku@gmail.com>
2020-01-28 21:37:16 +0100

    Feature: command cd works now

--- src/Terminal-Output/Output.tsx
+++ src/Terminal-Output/Output.tsx
@@ -1,5 +1,6 @@
 import '../App.css';
 import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
 export function Output(l: string[]){
     const items = l.map(item => <li><span className="user">user@Portfolio: </span><span className="input">{item}</span></li>)
@@ -10,11 +11,15 @@ export function Output(l: string[]){
 
 //We implement the folder structure as a simple dictionary!
 var folders :{[id:string]: string[];} = {
-    "": ["projects","blog","about.txt", "resume.txt"],
+    "": ["projects","blog"],
     "projects":[],
     "blog":[]
 }
-
+var files :{[id:string]: string[];} = {
+    "": ["about.txt","resume.txt"],
+    "projects":["exampleproject.txt"],
+    "blog":["exampleblog.txt"]
+}
 const help = <div>
     <li>Portfolio site of Edin Citaku, navigate with these commands:</li>
     <li className="help">    ls : displays the files in the current directory </li>
@@ -22,38 +27,68 @@ const help = <div>
     <li className="help">    cat [FILENAME] : outputs the content of the specified file </li>
 </div>
 
-export function executeCommand(input:string, currentFolder:string){
-        //Depending on our currentFolder and our input we 
-        if(input == "ls"){
-            
-            const folderlist = folders[currentFolder].map(item => {
-            
-                if(item.substr(item.length-3)=="txt"){
-                    return <div>{item}</div>
-                }
-                return <div className="folderview">{item}</div> 
-        })
-        return <div>{folderlist}</div>
+function executeSingleCommand(input:string, currentFolder:string):[any,string]{
+    //Depending on our currentFolder and our input we do our command
+    const inputSplit = input.split(" ");
+    if(inputSplit[0] == "ls"){
+        
+        const folderlist = folders[currentFolder].map(item => {
+            return <div className="folderview">{item}</div> 
+    })
+    const filelist = files[currentFolder].map(item => {
+        return <div className="fileview">{item}</div>
+    })
+
+        return [<div>{folderlist}{filelist}</div>, currentFolder]
         
     }
-    if(input == "help")
+    if(inputSplit[0] == "help")
     {
-        return help
+        if(inputSplit.length>1) return [<div>Help does not take any arguments!</div>,""]
+        return [help,currentFolder]
+    }
+    if(inputSplit[0] == "cd")
+    {
+        if(inputSplit.length == 1)
+        {
+            return [<div>Need to specify an foldername!</div>, currentFolder]
+        }
+        if(inputSplit.length >2)
+        {
+            return [<div>Too many arguments!</div>, currentFolder]
+        }
+        //We check if the input argument exists as a directory in our current position
+        if(folders[currentFolder].indexOf(inputSplit[1])>=0)
+        {
+            return [<div></div>,inputSplit[1]]
+        }
+        if(inputSplit[1]=="..")
+        {
+            return [<div></div>,""]
+        }
+        return [<div>Could not find folder {inputSplit[1]} </div>,currentFolder]
+
     }
 
 
-        return <div>DO NOT KNOW THAT COMMAND YET!</div>
+        return [<div>DO NOT KNOW THAT COMMAND YET!</div>,currentFolder]
+}
+function executeCommandList(inputList:string[])
+{
+    const output = []
+    var currentFolder = "";
+    for(let input of inputList)
+    {
+        const temp = executeSingleCommand(input,currentFolder)
+        currentFolder = temp[1];
+        const newelement = <li><span className="user">user@Portfolio: {currentFolder}: </span>{input} <span className="input">{temp[0]}</span></li>
+        output.push(newelement);
+
     }
-    
-    export function OutputTerminal(inputList:string[]){
+    return output;
+}
 
-    const items = inputList.map(item => {
-        return <div>
-    <li><span className="user">user@Portfolio: </span><span className="input">{item}</span></li>
-    <li>{executeCommand(item,"")}</li>
-    </div>
-})
-        return <div>{items}</div>
-
-    }
-
+export function OutputTerminal(inputList:string[])
+{
+    return executeCommandList(inputList)
+    }   
